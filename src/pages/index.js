804fff14614aec3f809271cfbb6b35321d206b1e
laughface809 <akhmarullah.techno@gmail.com>
2021-05-02 05:08:09 +0800

    first layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,44 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+import { Container, Row, Col } from "react-bootstrap"
+import dotaV2 from "../videos/dotav2.mp4"
+import dotaAd from "../audio/airport-bell.mp3"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <Container>
+      <Row>
+        <Col lg={6}> <p><strong>Denpasar Open Data</strong></p>
+        <video className="embed-responsive embed-responsive-16by9" controls>
+          <source className="embed-responsive-item" src={dotaV2} type="video/mp4" />
+        </video>
+        </Col>
+        <Col lg={6}><p><strong>Youtube Embeded</strong></p>
+          <div className="embed-responsive embed-responsive-16by9" controls>
+            <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/CmfC7OQSvEU"
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen></iframe>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6}> <p><strong>Denpasar Open Data</strong></p>
+          <video className="embed-responsive embed-responsive-16by9" controls>
+            <source className="embed-responsive-item" src={dotaV2} type="video/mp4" />
+          </video>
+        </Col>
+        <Col lg={6}><p><strong>Youtube Embeded</strong></p>
+          <audio className="embed-responsive embed-responsive-16by9" controls>
+            <source className="embed-responsive-item" src={dotaAd} type="audio/mp3" />
+          </audio>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 )
 
